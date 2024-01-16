c494e137a2c36d336a3b3fc35ebad7cd3673f8c1
Mdlalose4life <blessing1059@gmail.com>
2024-01-16 13:17:35 +0200

    The urls

--- client/src/components/Chats/Chat.js
+++ client/src/components/Chats/Chat.js
@@ -19,7 +19,7 @@ import io from 'socket.io-client';
 import logo from '../../assets/sbu.png'
 import { LuSendHorizonal } from "react-icons/lu";
 
-const API_URL =  'http://localhost:3330/';
+const API_URL =  'https://spiritlink.onrender.com/';
 
 function Chat({ rooms }) {
   const [newMessage, setNewMessage] = useState('');
