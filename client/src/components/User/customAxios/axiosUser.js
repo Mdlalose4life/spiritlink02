ff392e91fc2165037c6881e67e0b0776aff465f2
sbusisoShaper <sbusisomdlalose@thedigitalacademy.co.za>
2025-04-17 21:22:49 +0200

    adding the right url

--- client/src/components/User/customAxios/axiosUser.js
+++ client/src/components/User/customAxios/axiosUser.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customAxios = axios.create({
-  baseURL: 'https://spiritlink01-production.up.railway.app/'
+  baseURL: 'https://spiritlink02-production.up.railway.app/'
 });
 
 customAxios.interceptors.request.use(
